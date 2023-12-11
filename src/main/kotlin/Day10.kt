31f6e67253708c2c261c9828c718c4bafd6e9165
Ole-Petter Olsen <opolsen@gmail.com>
2023-12-11 21:16:48 +0100

    feat: Temporary solution for Day 10 part 2
    Current solution fails because it cannot handle squeezing between
    pipes.

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -8,6 +8,30 @@ class Day10 {
         return Math.floorDiv(loop.size, 2)
     }
 
+    fun solve2(lines: List<String>): Int {
+        val loop = findLoop(lines).toSet()
+        val maxY = lines.size - 1
+        val maxX = lines.first().count() - 1
+        val todo = mutableSetOf<Pair<Int, Int>>()
+        val checked = mutableSetOf<Pair<Int, Int>>()
+        val unenclosed = mutableSetOf<Pair<Int, Int>>()
+        unenclosed.addAll(getInitialUnenclosed(maxX, maxY, loop))
+        todo.addAll(unenclosed)
+        while (todo.isNotEmpty()) {
+            val newTodos = mutableSetOf<Pair<Int, Int>>()
+            todo.removeAll { it in checked }
+            todo.forEach { p ->
+                if (p !in loop) {
+                    unenclosed.add(p)
+                    newTodos.addAll(p.getAdjacent(maxX, maxY))
+                }
+                checked.add(p)
+            }
+            todo.addAll(newTodos)
+        }
+        return (maxX+1)*(maxY+1) - unenclosed.size - loop.size
+    }
+
     private fun findLoop(lines: List<String>): List<Pair<Int, Int>> {
         val yStart = lines.indexOfFirst { it.contains('S') }
         val xStart = lines[yStart].indexOfFirst { it == 'S' }
@@ -71,5 +95,42 @@ class Day10 {
         }
         throw Error("Could not find next part of loop")
     }
-}
 
+    private fun getInitialUnenclosed(maxX: Int, maxY: Int, loop: Set<Pair<Int, Int>>): Set<Pair<Int, Int>> {
+        val unenclosed = mutableSetOf<Pair<Int, Int>>()
+        (0..maxX).forEach {
+            if (Pair(it, 0) !in loop) {
+                unenclosed.add(Pair(it, 0))
+            }
+            if (Pair(it, maxY) !in loop) {
+                unenclosed.add(Pair(it, maxY))
+            }
+        }
+        (0..maxY).forEach {
+            if (Pair(0, it) !in loop) {
+                unenclosed.add(Pair(0, it))
+            }
+            if (Pair(maxX, it) !in loop) {
+                unenclosed.add(Pair(maxX, it))
+            }
+        }
+        return unenclosed
+    }
+
+    private fun Pair<Int, Int>.getAdjacent(maxX: Int, maxY: Int): List<Pair<Int, Int>> {
+        val adjacent = mutableListOf<Pair<Int, Int>>()
+        if (first > 0) {
+            adjacent.add(Pair(first - 1, second))
+        }
+        if (first < maxX) {
+            adjacent.add(Pair(first + 1, second))
+        }
+        if (second > 0) {
+            adjacent.add(Pair(first, second - 1))
+        }
+        if (second < maxY) {
+            adjacent.add(Pair(first, second + 1))
+        }
+        return adjacent
+    }
+}
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test
 
 class Day10Test {
     @Test
-    fun testSolve1a() {
+    fun testSolve1() {
         val input = """
             .....
             .S-7.
@@ -40,4 +40,60 @@ class Day10Test {
         val solution = Day10().solve1(input)
         assertEquals(8, solution)
     }
+
+    @Test
+    fun testSolve2a() {
+        val input = """
+            ...........
+            .S-------7.
+            .|F-----7|.
+            .||.....||.
+            .||.....||.
+            .|L-7.F-J|.
+            .|..|.|..|.
+            .L--J.L--J.
+            ...........
+        """.trimIndent().lines()
+        val solution = Day10().solve2(input)
+        assertEquals(4, solution)
+    }
+
+    @Test
+    fun testSolve2b() {
+        val input = """
+            .F----7F7F7F7F-7....
+            .|F--7||||||||FJ....
+            .||.FJ||||||||L7....
+            FJL7L7LJLJ||LJ.L-7..
+            L--J.L7...LJS7F-7L7.
+            ....F-J..F7FJ|L7L7L7
+            ....L7.F7||L7|.L7L7|
+            .....|FJLJ|FJ|F7|.LJ
+            ....FJL-7.||.||||...
+            ....L---J.LJ.LJLJ...
+        """.trimIndent().lines()
+        val solution = Day10().solve2(input)
+        assertEquals(8, solution)
+    }
+
+    @Test
+    fun testSolve2c() {
+        val input = """
+            FF7FSF7F7F7F7F7F---7
+            L|LJ||||||||||||F--J
+            FL-7LJLJ||||||LJL-77
+            F--JF--7||LJLJ7F7FJ-
+            L---JF-JLJ.||-FJLJJ7
+            |F|F-JF---7F7-L7L|7|
+            |FFJF7L7F-JF7|JL---7
+            7-L-JL7||F7|L7F-7F7|
+            L.L7LFJ|||||FJL7||LJ
+            L7JLJL-JLJLJL--JLJ.L
+        """.trimIndent().lines()
+        val solution = Day10().solve2(input)
+        assertEquals(10, solution)
+    }
+
+
+
 }

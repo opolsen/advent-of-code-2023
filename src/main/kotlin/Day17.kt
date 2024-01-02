6894043376a2564597deb352931905b42efdd7b3
Ole-Petter Olsen <opolsen@gmail.com>
2024-01-02 08:50:20 +0100

    refactor: Reuse solution for both parts

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,37 +1,13 @@
-import kotlin.math.min
-
 class Day17 {
     fun solve1(lines: List<String>): Int {
-        val positions = mutableSetOf<Pair<Int, Int>>()
-        val distances = mutableMapOf<Pair<Pair<Int, Int>, Pair<Direction, Int>>, Int>().withDefault { Int.MAX_VALUE }
-        lines.indices.forEach { y -> lines[0].indices.forEach { x -> positions.add(Pair(x, y)) } }
-        val initialNode = Node(Pair(0, 0), 0, mutableListOf())
-        val goal = Pair(lines.size - 1, lines[0].count() - 1)
-        val todos = mutableSetOf(initialNode)
-        while (todos.isNotEmpty()) {
-            val current = todos.minBy { it.distance }
-            if (current.pos == goal) {
-                return current.distance
-            }
-            getValidNeighbours(current, 0, 3).filter { it.first in positions }.forEach { neighbour ->
-                val tentativeDistance = current.distance + lines[neighbour.first.second][neighbour.first.first].digitToInt()
-                if (tentativeDistance < distances.getValue(neighbour)) {
-                    distances[neighbour] = tentativeDistance
-                    todos.add(
-                        Node(
-                            neighbour.first,
-                            tentativeDistance,
-                            current.path.plus(neighbour.second.first)
-                        )
-                    )
-                }
-            }
-            todos.remove(current)
-        }
-        throw Error("No solution found")
+        return getMinDistance(lines, 0, 3)
     }
 
     fun solve2(lines: List<String>): Int {
+        return getMinDistance(lines, 4, 10)
+    }
+
+    private fun getMinDistance(lines: List<String>, min: Int, max: Int): Int {
         val positions = mutableSetOf<Pair<Int, Int>>()
         val distances = mutableMapOf<Pair<Pair<Int, Int>, Pair<Direction, Int>>, Int>().withDefault { Int.MAX_VALUE }
         lines.indices.forEach { y -> lines[0].indices.forEach { x -> positions.add(Pair(x, y)) } }
@@ -42,11 +18,11 @@ class Day17 {
             val current = todos.minBy { it.distance }
             if (current.pos == goal) {
                 val currentDir = current.path.last()
-                if (current.path.reversed().take(4).count { it == currentDir } == 4) {
+                if (current.path.reversed().take(min).count { it == currentDir } == min) {
                     return current.distance
                 }
             }
-            getValidNeighbours(current, 4, 10).filter { it.first in positions }.forEach { neighbour ->
+            getValidNeighbours(current, min, max).filter { it.first in positions }.forEach { neighbour ->
                 val tentativeDistance = current.distance + lines[neighbour.first.second][neighbour.first.first].digitToInt()
                 if (tentativeDistance < distances.getValue(neighbour)) {
                     distances[neighbour] = tentativeDistance
